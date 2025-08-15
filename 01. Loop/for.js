10c0d1891654ad6a0369a9f22407951fb1a51cf5
developerujjal <ujjal.vision02@gmail.com>
2025-08-16 02:54:57 +0600

    forLoop basic

---
+++ 01. Loop/for.js
@@ -0,0 +1,19 @@
+// for(let number = 0; number <= 10; number++){
+//     console.log(number)
+// };
+
+
+// simple version
+// for(let i=0; i<=10; i++){
+//     console.log(i)
+// }
+
+
+// for(let i=0; i<=10; i+=2){
+//     console.log(i)
+// }
+
+
+for(let i=1; i<=10; i+=2){
+    console.log(i)
+}
