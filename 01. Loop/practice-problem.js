d108a8a949d7af1a69b697fc52934463b1152229
developerujjal <ujjal.vision02@gmail.com>
2025-08-17 03:53:43 +0600

    more simple practice problem

--- 01. Loop/practice-problem.js
+++ 01. Loop/practice-problem.js
@@ -20,6 +20,63 @@
 
 // reminder
 
-const number = 119;
-const reminder = number % 5;
-console.log(reminder)
+// const number = 119;
+// const reminder = number % 5;
+// console.log(reminder)
+
+
+// const fruits = ['Apple', "Banana", "Orange"];
+// const bananaIndex = fruits.indexOf('Banana');
+// fruits[bananaIndex] = "Mango";
+
+// console.log(fruits)
+
+// fruits.pop();
+
+// console.log(fruits)
+
+// fruits.push('Watermemon');
+// console.log(fruits)
+
+
+// const score = 100;
+
+// if (score <= 100 && score >= 80){
+//     console.log("Grade A")
+// } else if(score <= 80 && score >= 60){
+//     console.log('Grade B');
+// } else if(score <= 60 && score >= 50){
+//     console.log("Grade C");
+// } else if(score <= 50 && score >= 40){
+//     console.log("Grade D")
+// } else if(score <= 39 && score >= 1) {
+//     console.log("Grade F")
+// } else{
+//     console.log('Does not have any Grade, thanks')
+// }
+
+
+// const side1 = 5;
+// const side2 = 6;
+// const side3 = 5;
+
+// if(side1 === side2 || side1 === side3 || side2 === side3){
+//     console.log("isosceles")
+// }
+
+
+const num1 = 40;
+const num2 = 90;
+const num3 = 48;
+
+if(num1 > num2){
+    if(num1 > num3){
+        console.log(num1)
+    }else{
+        console.log(num3)
+    }
+}else{
+    if(num2 > num3){
+        console.log(num2)
+    }
+}
