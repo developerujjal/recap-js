8953a2a0403ed21d7a380cfd59d4a1518001464b
developerujjal <ujjal.vision02@gmail.com>
2025-08-17 03:24:56 +0600

    simple practice problem

---
+++ 01. Loop/practice-problem.js
@@ -0,0 +1,25 @@
+// const moneyGiven = 1000;
+// const appleCost = 400;
+// const orangeCost = 300;
+
+// const totalExpense = appleCost + orangeCost;
+// const remainingMoney = moneyGiven - totalExpense;
+// console.log(remainingMoney)
+
+
+// average
+
+// const mathMark = 75.25;
+// const biologyMark = 65;
+// const chemistryMark = 80;
+
+// const totalMarks = mathMark + biologyMark + chemistryMark;
+// const average = totalMarks / 3;
+// console.log(average.toFixed(2))
+
+
+// reminder
+
+const number = 119;
+const reminder = number % 5;
+console.log(reminder)
