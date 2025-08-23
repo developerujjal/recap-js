b1b06c9dc50023e14fa7f4d1bdf3ebd5fc670a44
developerujjal <ujjal.vision02@gmail.com>
2025-08-24 02:32:40 +0600

    basic about function

---
+++ 02. Core Concept Function and Object/function.js
@@ -0,0 +1,10 @@
+// Function declaration
+
+function study(){
+    console.log('Weak up');
+    console.log('Fresh your self first');
+    console.log('Start your Study stuff')
+};
+
+
+study()
---
+++ 02. Core Concept Function and Object/parameter.js
@@ -0,0 +1,5 @@
+function bringSingara(money){
+    console.log('i have total Tk:', money)
+};
+
+bringSingara(15)
